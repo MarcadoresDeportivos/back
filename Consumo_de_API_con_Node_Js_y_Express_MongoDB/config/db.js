2d43435bf33265c5543fe385eee2f8cf8dff9446
arteagabrayan <arteagabrayan99ah@gmail.com>
2022-09-28 18:25:45 -0500

    Desarrollo del Backend 4

--- Consumo_de_API_con_Node_Js_y_Express_MongoDB/config/db.js
+++ Consumo_de_API_con_Node_Js_y_Express_MongoDB/config/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Libreria para usar Mongo DB
 const mongoose = require("mongoose")
 // Dirección de la DB
@@ -9,23 +8,10 @@ module.exports = ()=>{
         mongoose.connect(
             URIDB,
             {
-=======
-const mongoose = require('mongoose');
-const DB_URI = 'mongodb://localhost:27017/marcadores';
-
-module.exports = () =>{
-
-    const conn = () =>{
-
-        mongoose.connect(
-
-            DB_URI,{
->>>>>>> 0bdd7b669da55def15ad9dcf2c8523bd8c021898
                 keepAlive : true,
                 useNewUrlParser : true,
                 useUnifiedTopology : true
             },
-<<<<<<< HEAD
             (err) => {
                 if(err){
                     console.log("Error Conec: "+err)
@@ -36,20 +22,4 @@ module.exports = () =>{
         )
     }
     conn()
-=======
-            (err) =>{
-                if(err){
-                    console.log("error DB: "+err);
-                }else{
-                    console.log("Conexion Satisfactoria");
-                }
-
-            }
-
-        )
-
-    }
-    conn();
-
->>>>>>> 0bdd7b669da55def15ad9dcf2c8523bd8c021898
 }
