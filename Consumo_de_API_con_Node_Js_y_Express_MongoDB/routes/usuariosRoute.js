7de7ead20f4f9ad3d4a87b265fab29221dc572ce
Harold Brayan Arteaga Arteaga <95327759+arteagabrayan@users.noreply.github.com>
2022-11-02 10:52:03 -0500

    Delete Consumo_de_API_con_Node_Js_y_Express_MongoDB directory

--- Consumo_de_API_con_Node_Js_y_Express_MongoDB/routes/usuariosRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const usuariosCtrl = require('../controllers/usuariosCtrl')
-
-router.post('/',usuariosCtrl.usuarioGuardar)
-router.post('/login/', usuariosCtrl.usuarioLogin)
-
-module.exports = router
